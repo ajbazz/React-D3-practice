31008b4237845f0bd91e3b95f6a210240b594104
Aaron Bazzone <ajbazz1@gmail.com>
2017-02-26 00:46:13 -0800

    Drag and drop working but project...not so much

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -17,7 +17,19 @@ class Grid extends React.Component {
 	    let click = 0;
 	    let rows = this.props.rows;
 	    let cols = this.props.cols;
-	
+		var dataset = [
+		  {x: 0, y: 0},
+		  {x: 1, y: 1},
+		  {x: 2, y: 2},
+		  {x: 3, y: 3},
+		  {x: 4, y: 4},
+		  {x: 5, y: 5},
+		  {x: 6, y: 6},
+		  {x: 7, y: 7},
+		  {x: 8, y: 8},
+		  {x: 9, y: 9},
+		  {x: 10, y: 10}
+		];
 	    // iterate for rows 
 	    for (let row = 0; row < rows; row++) {
 	        data.push( new Array() );
@@ -39,15 +51,58 @@ class Grid extends React.Component {
 	        // increment the y position for the next row. Move it down 50 (height variable)
 	        ypos += height; 
 	    }
-    	this.createGrid(data);
+	    
+	    
+    	this.createGrid(data, dataset);
 	}
 	
-	createGrid (data) {
-		console.log(data);
+	createGrid (data, dataset) {
+		const margin = {top: 40, right: 30, bottom: 20, left: 30};
+		const gridWidth = 560 - margin.left - margin.right;	
+		const gridHeight = 560 - margin.top - margin.bottom;
+	    
+		    
+		    try{
+	    let gData = data[0];
+		console.log(d3.max(dataset, function(d){return d.x}));
+		
+		let xScale = d3.scale.linear()
+		    .domain([0, d3.max(dataset, function(d){return d.x; })])
+		    .range([0, gridWidth]);
+	
+		let yScale = d3.scale.linear()
+	    	.domain([0, d3.max(dataset, function(d){ return d.y; })])
+	    	.range([gridHeight, 0]);
+			
+		let xAxis = d3.svg.axis()
+		    .scale(xScale)
+		    .orient("bottom")
+		    .innerTickSize(-gridHeight)
+		    .outerTickSize(0)
+		    .tickPadding(10);
+		    
+		let yAxis = d3.svg.axis()
+		    .scale(yScale)
+		    .orient("left")
+		    .innerTickSize(-gridWidth)
+		    .outerTickSize(0)
+		    .tickPadding(10);
+		
 		let grid = d3.select("#grid")
 			.append("svg")
-			.attr("width","510px")
-			.attr("height","510px");
+		    .attr("width", gridWidth + margin.left + margin.right)
+		    .attr("height", gridHeight + margin.top + margin.bottom)
+	  	.append("g")
+		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		    
+		grid.append("g")
+	      .attr("class", "x axis")
+	      .attr("transform", "translate(0," + gridHeight + ")")
+	      .call(xAxis);
+	
+		grid.append("g")
+	      .attr("class", "y axis")
+	      .call(yAxis);
 		
 		let row = grid.selectAll(".row")
 			.data(data)
@@ -57,7 +112,7 @@ class Grid extends React.Component {
 		let column = row.selectAll(".square")
 		    .data(function(d) { return d; })
 		    .enter().append("rect")
-		    .attr("class","square")
+		    .attr("class","square dropzone")
 		    .attr("x", function(d) { return d.x; })
 		    .attr("y", function(d) { return d.y; })
 		    .attr("width", function(d) { return d.width; })
@@ -70,11 +125,14 @@ class Grid extends React.Component {
 			   if ((d.click)%4 == 1 ) { d3.select(this).style("fill","#2C93E8"); }
 			   if ((d.click)%4 == 2 ) { d3.select(this).style("fill","#F56C4E"); }
 			   if ((d.click)%4 == 3 ) { d3.select(this).style("fill","#838690"); }
-		    });
+		   });
+		   }catch (err) {
+		   	console.log(err);
+		   }
 	}
 	
 	render() {
-		return <div className="jumbotron"><MarkerPane /><div id="grid" className={classes.grid}></div></div>
+		return <div className=""><MarkerPane /><div id="grid" className={classes.grid}></div></div>
 	}
 	
 	defaultProps = {
--- src/components/MarkerPane/MarkerPane.js
+++ src/components/MarkerPane/MarkerPane.js
@@ -1,9 +1,72 @@
 import React from 'react'
+// import Rd3 from 'rd3'
+import D3 from 'd3'
+import D3drag from 'd3-drag'
 import classes from './MarkerPane.scss'
 
 class MarkerPane extends React.Component {
+    componentDidMount () {
+		let markers = this.refs.markerPane.children;
+		let i = 0;
+		let mark = null;
+		let self = this;
+		let dragged = null;
+		
+		for (i; i < markers.length; i = i + 1) {
+			mark = markers[i];
+			mark.draggable = true;
+		}
+		document.addEventListener("drag", function( event ) {
+		}, false);
+		document.addEventListener('dragstart', function(e) {
+			dragged = e.target;
+  			dragged.style.opacity = .5;
+		}, false);
+		document.addEventListener("dragend", function( event ) {
+		      // reset the transparency
+		      event.target.style.opacity = "";
+		      if ( event.target.classList.contains("dropzone") ) {
+		          D3.select(event.target).style("fill","#FFFFFF");
+		      }
+		  }, false);
+		
+	  document.addEventListener("dragover", function( event ) {
+	      // prevent default to allow drop
+	      event.preventDefault();
+	  }, false);
+	
+	  document.addEventListener("dragenter", function( event ) {
+	      // highlight potential drop target when the draggable element enters it
+	      if ( event.target.classList.contains("dropzone") ) {
+	          D3.select(event.target).style("fill","#838690");
+	      }
+		}, false);	  
+		document.addEventListener("dragleave", function( event ) {
+	      // highlight potential drop target when the draggable element enters it
+	      if ( event.target.classList.contains("dropzone") ) {
+	          D3.select(event.target).style("fill","#FFFFFF");
+	      }
+		}, false);
+		document.addEventListener("drop", function( event ) {
+	      // prevent default action (open as link for some elements)
+	      event.preventDefault();
+	      // move dragged elem to the selected drop target
+	      if ( event.target.classList.contains("dropzone") ) {
+	          dragged.parentNode.removeChild( dragged );
+	          event.target.appendChild( dragged );
+	          D3.select(event.target).style("fill","red");
+	      }
+		}, false);
+	}
+	
+	drag (evt) {
+		console.log(evt.dataTransfer);
+    	evt.dataTransfer.setData("text", evt.target.id);
+	}
+	
 	render () {
-		return <div className={classes.markerPane}><div className={classes.marker}>9, 1</div> <div className={classes.marker}>6, 5</div> <div className={classes.marker}>4, 2</div> <div className={classes.marker}>3, 8</div></div>
+		return <div className={classes.markerPane} ref="markerPane"><div className={classes.marker}><div data-x="201" data-y="1">x=201<br/>y=1</div></div> <div className={classes.marker} data-x="351" data-y="351"><div>x=351<br/>y=351</div></div> <div className={classes.marker} data-x="51" data-y="151"><div>x=51<br/>y=151</div></div> <div className={classes.marker} data-x="251" data-y="401"><div>x=251<br/>y=401</div></div></div>
+		
 	}
 }
 
